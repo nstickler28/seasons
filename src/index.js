3d91bcbf469180ed30e888e59e69f9327cabb163
nstickler28 <nathanstickler@gmail.com>
2022-06-15 14:51:00 -0700

    Added API to get users geographical location

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        // successful callback
+        (position) => console.log(position),
+        // failed callback
+        (error) => console.log(error)
+    );
     return <div>Hi there!</div>;
 };
 
