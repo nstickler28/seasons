e2361b39a72f53ad61605d2173ccbff6ec70ed8c
nstickler28 <nathanstickler@gmail.com>
2022-06-15 15:16:07 -0700

    defined constructor and initialized state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+    constructor(props) {
+        // reference to the parents constructor function
+        super(props);
+
+        this.state = { lat: null };
+    }
+
+    // React says we have to define render!!
     render() {
         window.navigator.geolocation.getCurrentPosition(
             // successful callback
