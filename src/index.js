7a84ab2424ce98f652e64bb92ae094013d896276
nstickler28 <nathanstickler@gmail.com>
2022-06-16 12:25:32 -0700

    Added loading file and loading icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Loading from './Loading';
 
 class App extends React.Component {
     // refactored and shortened so we don't have to declare constructor and call super
@@ -46,7 +47,7 @@ class App extends React.Component {
         if (!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />
         }
-        return <div>Loading...</div>
+        return <Loading />
     }
 }
 
