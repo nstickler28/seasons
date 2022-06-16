0a88104f0653f5d8e36f132cfa6654b557fc32ea
nstickler28 <nathanstickler@gmail.com>
2022-06-16 11:48:28 -0700

    Added right season icons from semantics ui library

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,16 +2,23 @@ import React from 'react';
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
-        return lat > 0 ? 'summer' : 'winter';
+        return lat > 0 ? 'Summer' : 'Winter';
     } else {
-        return lat > 0 ? 'winter' : 'summer';
+        return lat > 0 ? 'Winter' : 'Summer';
     }
 };
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    console.log(season);
-    return <div>Season Display</div>;
+    const text = season === 'Winter' ? 'Burr, it is chilly' : 'Lets hit the beach!';
+    const icon = season === 'Winter' ? 'snowflake' : 'sun';
+    return (
+        <div>
+            <i className={`${icon} icon`} />
+            <h1>{text}</h1>
+            <i className={`${icon} icon`} />
+        </div>
+    )
 };
 
 export default SeasonDisplay;
